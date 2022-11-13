e1a70ec326665751ef0c6ae688b5ce5887ebaf43
ekk <erik.koch.g@gmail.com>
2022-11-13 13:16:45 +0300

    v0

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// database config [config.php] made by ekk v1.0
-	$dbBaseName = "database name";
-	$dbUserName = "database username";
-	$dbPassword = "database password";
-	$dbHostName = "database hostname";
-?>
