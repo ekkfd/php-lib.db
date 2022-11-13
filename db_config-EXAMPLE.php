3e50226e6d9e6f05a5797b537426ac4a89a59d58
ekk <erik.koch.g@gmail.com>
2022-11-13 15:12:16 +0300

    v0.115 db_config fix

--- db_config-EXAMPLE.php
+++ db_config-EXAMPLE.php
@@ -1,11 +1,17 @@
 <?php
 	// database config [db_config.php] made by ekkfd, dkh-gh
 
-	$db_conf = [
+	$db_config = [
 		"basename" => "database name",
 		"username" => "database username",
 		"password" => "database password",
 		"hostname" => "database hostname",
-	];
 
+		"sets" => [
+			"SET NAMES 'utf8';",
+			"SET CHARACTER SET 'utf8';",
+			"SET SESSION collation_connection = 'utf8_general_ci';",
+		],
+	];
+	
 ?>
